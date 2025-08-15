073f7449a5e60e2fcc798a362d9d316ad8073790
mbathaedward <mbathaedward11@gmail.com>
2025-08-15 12:05:34 +0300

    added addtrasctions

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ const amount = document.getElementById("amount");
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
 //addtodom
-function addToDom(transactions){
-    const sign = transactions.amount < 0 ? '-' : "+";
+function addToDOM(transaction){
+    const sign = transaction.amount < 0 ? '-' : "+";
     const item = document.createElement("li");
 
     item.classList.add(transactions.amount < 0 ? "minus" : "plus");//add a css class minus if amout is - otherwise +
 
     item.innerHTML = 
-    `${transactions.text}
-    <span>${sign}$${Math.abs(transactions.amount).toFixed(2)}</span>
-    <button class="delete-btn" onclick="removeTransaction(${transactions.id}">x</button>`;
+    `${transaction.text}
+    <span>${sign}ksh ${Math.abs(transaction.amount).toFixed(2)}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>`;
     list.appendChild(item);
 
 }
@@ -28,12 +28,12 @@ function addToDom(transactions){
 function updateValues(){
     const amounts = transactions.map(t => t.amount);//go through each trrasaction obj.
     const total = amounts.reduce((acc, val )=> acc + val, 0 ).toFixed(2); //adds up all numbers starting 0
-    const income = amounts.filter(a => a > 0).reduce((acc, val )=> acc, val , 0).toFixed(2);//finds all positive amouts and sum them
+    const income = amounts.filter(a => a > 0).reduce((acc, val )=> acc + val , 0).toFixed(2);//finds all positive amouts and sum them
     const expense = Math.abs(amounts.filter(a => a < 0).reduce((acc, val ) => acc + val, 0)).toFixed(2);//finds all negatives and sums them
     // updates ui to uses see them n browser
-    balance.innerText = `$${total}`;
-    moneyPlus.innerText = `+$${income}`;
-    moneyMinus.innerText = `$${expense}`;
+    balance.innerText = ` ksh ${total}`;
+    moneyPlus.innerText = `+ksh ${income}`;
+    moneyMinus.innerText = `ksh ${expense}`;
 
 }
 //functions delete transactions
@@ -45,9 +45,33 @@ function removeTransaction(id){
 function updateLocalStorage(){
     localStorage.setItem("transactions", JSON.stringify(transactions));
 }
-function generateId(){
+function generateID(){
     return Math.floor(Math.random() * 1000000)
 }
 
-
-
+//fuction add submit form
+function addTrasaction(e){
+    e.preventDefault()
+    if (text.value.trim() === '' || amount.value.trim() === ''){
+        alert("Please Enter a text and amount");
+        return;
+    }
+    const transaction = {
+        id:generateID(),
+        text: text.value,
+        amount: +amount.value
+    }
+    transactions.push(transaction);
+    addToDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+    text.value = '';
+    amount.value = '';
+}
+ function init(){
+    list.innerHTML = "";
+    transactions.forEach(addToDOM);
+    updateValues()
+}
+init();
+form.addEventListener("submit",addTrasaction);
